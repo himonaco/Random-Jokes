1f0d07ff72abb86a02465d6c63b4e10498d6937d
Luca Monaco <lucamonaco@icloud.com>
2024-01-19 13:49:55 +0100

    jokes

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
+// Write your code here
+
 function getJokes(){
     fetch('https://backend-omega-seven.vercel.app/api/getjoke')
     .then(response => response.json())
     .then(data =>{
-        let questions = "";
-        let punchlines = "";
-        for (let joke of data) {
-            questions += joke.question;
-            punchlines += joke.punchline;
-        }
-        document.querySelector('#result-question').textContent = questions;
-        document.querySelector('#result-punchline').textContent = punchlines;
+        document.querySelector('#result-question').textContent = data[0].question;
+        document.querySelector('#result-punchline').textContent = data[0].punchline;
     })
-}
-
-document.querySelector('#fetch-btn').addEventListener('click', getJokes);
+    }
+    
+    document.querySelector('#fetch-btn').addEventListener('click',function(){
+        getJokes();
+    });
+    
+    
